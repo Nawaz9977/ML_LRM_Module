6438f9fb9525f032836f606b386aa2b58925e18b
Shahanavaj <shahanavajansari@gmail.com>
2023-08-18 16:32:13 +0530

    Sami-->> Commit

--- data/data.py
+++ data/data.py
@@ -1,3 +1,3 @@
 # You can implement this later
 # You can store your csv file or Dataset file etc. but for now we are placing 
-#csv file(boston_housing.csv)in pipeline folder
+#csv file(boston_housing.csv)in pipeline folder 
